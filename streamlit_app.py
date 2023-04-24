24319e52f767fddb602472374a07cd534fdc6b34
harishvtcs <131663275+harishvtcs@users.noreply.github.com>
2023-04-24 15:44:10 +0530

    Update streamlit_app.py
    added new texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit;
 streamlit.title('My New Healthy Dinner');
+steramlit.header('Breakfast Menu');
+Streamlit.text('Omega 3 & Blueberry Oat meal')
+Streamlit.text('Kale , SPinach & Rocket Smoothie')
+Streamlit.text('Hard Boiled Free Range egg')
